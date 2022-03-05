819611d0895715b7e5ab83771dc763f08a35e8bd
Nallely Reyes <na_reyesd@unicah.edu>
2022-03-05 02:34:24 -0600

    Pantalla de configuraciones

--- ferreteria/App.js
+++ ferreteria/App.js
@@ -1,29 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-
+import Configuraciones from './src/componentes/configuraciones';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Pressable style={styles.boton}>Click me</Pressable>
-
-    </View>
+  <Configuraciones></Configuraciones>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boton:
-  {
-    backgroundColor: '#495057',
-    padding: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
