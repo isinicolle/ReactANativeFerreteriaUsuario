8f53e5f73753e9a42aa4219b68165666f716113f
Javier C <acej2207@gmail.com>
2022-03-05 20:37:23 -0600

    cambios a pantalla info

--- ferreteria/src/componentes/index.js
+++ ferreteria/src/componentes/index.js
@@ -4,11 +4,14 @@ import PasswordBox from './PasswordBox'
 import TextBox from './TextBox'
 import Footer from './Footer'
 import Header from './Header'
+import Texts from './Text'
+
 export {
 HiperVinculo,
 Boton,
 PasswordBox,
 TextBox,
 Footer,
-Header
+Header,
+Texts,
 }
