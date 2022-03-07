6e41b7da381f66099d50c39d175a9aeb933f1ca2
Nallely Reyes <na_reyesd@unicah.edu>
2022-03-06 19:12:13 -0600

    cambio pant

--- ferreteria/App.js
+++ ferreteria/App.js
@@ -3,12 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { ScrollView,TextInput, StyleSheet, Text, View,Image,SafeAreaView} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import Pantalla from './src/Panallas/direcciones'
+import Configuraciones from './src/Panallas/informacionUsuario'
 
 export default function App() {
   return (
     <ScrollView style={{flex:1}}>
-      <Pantalla />
+      <Configuraciones />
 
     </ScrollView>
     
